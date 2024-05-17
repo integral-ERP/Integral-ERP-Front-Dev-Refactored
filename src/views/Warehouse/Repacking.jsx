338f93c3d976b97639e445a472371a3c4685b452
isabellamarin09 <isabella09m2002@gmail.com>
2024-05-17 13:17:25 -0500

    Diseño repacking

--- src/views/Warehouse/Repacking.jsx
+++ src/views/Warehouse/Repacking.jsx
@@ -245,15 +245,13 @@ const Repacking = () => {
              {/*  fix the repacking show internalCommodities for edition */}
              {selectedCommodity.internalCommodities &&
                selectedCommodity.internalCommodities.map((com) => (
-                <div key={com.id} className="card" style={{ display: 'flex', textAlign: 'left', fontSize: '15px' }}>
+                <div key={com.id} className="card" style={{ display: 'flex', textAlign: 'left', fontSize: '15px', flexDirection: 'row'}}>
                    <p className="item-description">{com.description}</p>
-                   <p className="item-info">Length (in): {com.length}</p>
+                   <p className="item-info"  style={{ marginLeft: '50px'}}>Length (in): {com.length}</p>
                    <p className="item-info">Width(in): {com.width}</p>
-                   <p className="item-info">Height (in): {com.height}</p>
+                   <p className="item-info" style={{ marginLeft: '43px'}}>Height (in): {com.height}</p>
                    <p className="item-info">Weight (lb): {com.weight}</p>
-                   
-                   
-                   
+
                    <p className="item-info">
                      {/* Volumetric-1 Weight: {com.volumetricWeight} */}
                      Volume (ft3): {com.volumetricWeight}
