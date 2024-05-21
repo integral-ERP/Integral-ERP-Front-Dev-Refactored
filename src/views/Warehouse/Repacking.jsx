0eb8e12e2ef9ff795e0b6ce06a5b9f8b55168ed3
isabellamarin09 <isabella09m2002@gmail.com>
2024-05-21 09:59:17 -0500

    Repacked commodities

--- src/views/Warehouse/Repacking.jsx
+++ src/views/Warehouse/Repacking.jsx
@@ -245,11 +245,11 @@ const Repacking = () => {
              {/*  fix the repacking show internalCommodities for edition */}
              {selectedCommodity.internalCommodities &&
                selectedCommodity.internalCommodities.map((com) => (
-                <div key={com.id} className="card" style={{ display: 'flex', textAlign: 'left', fontSize: '15px', flexDirection: 'row'}}>
+                <div key={com.id} className="card" style={{ display: 'flex', textAlign: 'left', fontSize: '15px', flexDirection: 'row', margin: '0px'}}>
                    <p className="item-description">{com.description}</p>
-                   <p className="item-info"  style={{ marginLeft: '50px'}}>Length (in): {com.length}</p>
+                   <p className="item-info">Length (in): {com.length}</p>
                    <p className="item-info">Width(in): {com.width}</p>
-                   <p className="item-info" style={{ marginLeft: '43px'}}>Height (in): {com.height}</p>
+                   <p className="item-info">Height (in): {com.height}</p>
                    <p className="item-info">Weight (lb): {com.weight}</p>
 
                    <p className="item-info">
